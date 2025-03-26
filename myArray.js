c457bf34196c9acb7266eeebf302dc7daf763d87
joemcdonough15 <joemcdonoughmusic@gmail.com>
2025-03-26 01:13:16 -0400

    write method to push items to MyArray

--- myArray.js
+++ myArray.js
@@ -14,9 +14,13 @@ class MyArray {
     return this; // return statement for chaining this method to a newly created MyArray object
   };
 
-  pushMyArray = (itemToAdd) => {
-    // increment lengthMyArray by 1
-    // index this.array at sizeOfArray - 1 and set it to itemToAdd
+  pushMyArray = (...itemsToAdd) => {
+    this.lengthMyArray += itemsToAdd.length;
+    let startingIndex = this.lengthMyArray - itemsToAdd.length;
+    for (let i = 0; i < itemsToAdd.length; i++) {
+      this.array[startingIndex] = itemsToAdd[i];
+      startingIndex++;
+    }
   };
 
   unshiftMyArray = (itemToAdd) => {
@@ -34,22 +38,38 @@ class MyArray {
 // * Tests to create new array
 
 // const myArr = new MyArray(); // create an empty array
-// const myArr2 = new MyArray(3); // create an array of size 3
 // console.log(myArr.array); // []
+
+// const myArr2 = new MyArray(3); // create an array of size 3
 // console.log(myArr2.array); // [undefined, undefined, undefined]
 
 // * Tests to fill all indices of array with a given value
 
 // * calling .fillMyArray on MyArray objects after they are instantiated
-const myArr = new MyArray();
-const myArr2 = new MyArray(3);
-myArr.fillMyArray("abc");
-console.log(myArr.array); // []
-myArr2.fillMyArray(null);
-console.log(myArr2.array); // [null, null, null]
+// const myArr = new MyArray();
+// myArr.fillMyArray("abc");
+// console.log(myArr.array); // []
+
+// const myArr2 = new MyArray(3);
+// myArr2.fillMyArray(null);
+// console.log(myArr2.array); // [null, null, null]
 
 // * calling .fillMyArray by chaining it to the newly created MyArray object - return statement on line 14 ensures the MyArray object gets returned and assigned to the variable.
 // const myArr = new MyArray().fillMyArray("abc"); // filling an empty array
 // console.log(myArr.array); // []
+
 // const myArr2 = new MyArray(3).fillMyArray(null); // more practical example by filling 3 indices of the array with null. Mimicking const myArr2 = new Array(3).fill(null)
 // console.log(myArr2.array); // [null, null, null]
+
+// * Tests to push items to MyArray
+
+const myArr = new MyArray();
+myArr.pushMyArray(5);
+console.log(myArr.array); // [5]
+
+const myArr2 = new MyArray(3);
+myArr2.pushMyArray(6);
+console.log(myArr2.array); // [undefined, undefined, undefined, 6]
+
+myArr2.pushMyArray(7, 8, 9, 10); // pushMyArray with multiple arguments at once
+console.log(myArr2.array); // [undefined, undefined, undefined, 6, 7, 8, 9, 10]
